e9670282f189fffea7c2c0946f8b2c0168d5bdc6
donkaec42 <abogus@gmail.com>
2019-02-04 08:46:21 -0800

    new script

---
+++ devonc42.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  Donkaec42;
+git config --global user.name  donkaec42;
+git config --global user.user donkaec42;
+git config --global user.email abogus@gmail.com;
+git config --list;
